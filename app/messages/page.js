bfc0207c38744a9c769cb433d8d92cc97209853e
HanyMohamed <hsaad9231@gmail.com>
2024-06-02 20:57:41 +0300

    learn caching

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,9 +1,9 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
+  const response = await fetch("http://localhost:8080/messages", {
+    next: {
+      revalidate: 5,
     },
   });
   const messages = await response.json();
